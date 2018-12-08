409a6a06ad8f727c3bd694818ed0b752b50d6e85
cedesu <34418825+cedesu@users.noreply.github.com>
2018-12-08 12:11:41 -0500

    Update train_segmentation.py

--- train_segmentation.py
+++ train_segmentation.py
@@ -17,42 +17,38 @@ from datasets import PartDataset
 from model import Modified3DUNet
 import torch.nn.functional as F
 
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--batchSize', type=int, default=32, help='input batch size')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=1)
 parser.add_argument('--nepoch', type=int, default=25, help='number of epochs to train for')
-parser.add_argument('--outf', type=str, default='seg',  help='output folder')
-parser.add_argument('--model', type=str, default = '',  help='model path')
-
+parser.add_argument('--outf', type=str, default='seg', help='output folder')
+parser.add_argument('--model', type=str, default='', help='model path')
 
 opt = parser.parse_args()
-print (opt)
+print(opt)
 
-opt.manualSeed = random.randint(1, 10000) # fix seed
+opt.manualSeed = random.randint(1, 10000)  # fix seed
 print("Random Seed: ", opt.manualSeed)
 random.seed(opt.manualSeed)
 torch.manual_seed(opt.manualSeed)
 
-dataset = PartDataset(root = '..//sdf_newnew',train=True)
+dataset = PartDataset(root='..//sdf_newnew', train=True)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
-                                          shuffle=True, num_workers=int(opt.workers))
+                                         shuffle=True, num_workers=int(opt.workers))
 
-test_dataset = PartDataset(root = '..//sdf_newnew',train=False)
+test_dataset = PartDataset(root='..//sdf_newnew', train=False)
 testdataloader = torch.utils.data.DataLoader(test_dataset, batch_size=opt.batchSize,
-                                          shuffle=True, num_workers=int(opt.workers))
+                                             shuffle=True, num_workers=int(opt.workers))
 
-print(len(dataset),len(test_dataset))
+print(len(dataset), len(test_dataset))
 try:
     os.makedirs(opt.outf)
 except OSError:
     pass
 
-blue = lambda x:'\033[94m' + x + '\033[0m'
+blue = lambda x: '\033[94m' + x + '\033[0m'
 
-
-classifier = Modified3DUNet(in_channels=1,n_classes=1)
+classifier = Modified3DUNet(in_channels=1, n_classes=1)
 
 if opt.model != '':
     classifier.load_state_dict(torch.load(opt.model))
@@ -62,25 +58,26 @@ classifier.cuda()
 
 loss_fn = torch.nn.MSELoss(reduce=False, size_average=False)
 
-num_batch = len(dataset)/opt.batchSize
+num_batch = len(dataset) / opt.batchSize
 
 for epoch in range(opt.nepoch):
     for i, data in enumerate(dataloader, 0):
         points, target = data
-        print(points.size())
         points, target = Variable(points), Variable(target)
-        points, target = points.cuda(), target.cuda()   
+        points = points.transpose(2, 1)
+        points, target = points.cuda(), target.cuda()
         optimizer.zero_grad()
         classifier = classifier.train()
         pred = classifier(points)
         pred = pred.view(-1)
         target = target.view(-1)
-        #print(pred.size(), target.size())
+        # print(pred.size(), target.size())
         loss = loss_fn(pred, target)
+        printf(loss.type(),loss.size(),loss)
         loss.backward()
         optimizer.step()
-        print('[%d: %d/%d] train loss: %f loss: %f' %(epoch, i, num_batch, loss.item(), loss))
-        
+        print('[%d: %d/%d] train loss: %f loss: %f' % (epoch, i, num_batch, loss.item(), loss))
+
         '''if i % 10 == 0:
             j, data = next(enumerate(testdataloader, 0))
             points, target = data
