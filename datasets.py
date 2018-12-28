8a7257ddb30322cec497a336b8e07646fe0325cb
cedesu <34418825+cedesu@users.noreply.github.com>
2018-12-28 10:38:43 +0800

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -58,7 +58,7 @@ class PartDataset(data.Dataset):
         in_voxel = in_voxel.float()
         out_sdf = out_sdf.float()
 
-        return in_voxel，out_sdf
+        return in_voxel,out_sdf
 
     def __len__(self):
         return len(self.path)
